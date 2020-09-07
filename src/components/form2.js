47664f0285921dac8243411509059580e868a3ad
rasoulm777 <rasoulm777@gmail.com>
2020-09-07 22:09:29 +0430

    Add Loading

--- src/components/form2.js
+++ src/components/form2.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import Loading from './loading'
 import './form.css'
 export default class Form2 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: [],
+      users: null,
+      isLoading: true
     };
   }
   componentDidMount() {
     Axios.get("https://api.jsonbin.io/b/5f553ee3993a2e110d3fc004").then((res) => {
-      this.setState({users:res.data})
+      this.setState({ users: res.data })
       console.log(this.state.users.name);
     });
   }
+
   render() {
     return (
       <>
@@ -24,18 +27,22 @@ export default class Form2 extends Component {
           </p>
 
           <div>
-            {this.state.users.map((item) => {
-              return (
-                <div key={item.id}>
-                  <p>نام مدرسه : {item.name}</p>
-                  <p>مفطع : {item.maq}</p>
-                  <p>نام مدیر : {item.mname}</p>
-                  <p>شماره موبایل مدیر : {item.mob}</p>
-                  <p>آدرس مدرسه : {item.address}</p>
-                  <p>کد پستی : {item.code}</p>
-                </div>
+
+            {this.state.users !== null ?
+
+              this.state.users.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <p>نام مدرسه : {item.name}</p>
+                    <p>مفطع : {item.maq}</p>
+                    <p>نام مدیر : {item.mname}</p>
+                    <p>شماره موبایل مدیر : {item.mob}</p>
+                    <p>آدرس مدرسه : {item.address}</p>
+                    <p>کد پستی : {item.code}</p>
+                  </div>
               );
-            })}
+                 }):<Loading/>
+          }
           </div>
         </div>
       </>
---
+++ src/components/loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './loading.css'
+
+function loading() {
+    return(
+        <div className='loading'></div>
+
+    )
+}
+export default loading;
